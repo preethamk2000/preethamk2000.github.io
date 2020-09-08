ae8ab80b00f794524150c7f88112234e0618d243
P K <preethamk18042000@gmail.com>
2020-09-08 22:57:12 +0530

    Update image, refactor code
    Added full border to image, made typeanimation.js a little bit prettier, removed unnecessary css and added Pickle Rick to the party

--- js/typeanimation.js
+++ js/typeanimation.js
@@ -1,55 +1,58 @@
-var i = 0,j=0;
+var i = 0,j = 0;
 var txt_arr = [" Hi! I'm Preetham ",
                " Junior CS UG ",
                " S/W Developer ",
                " ML/DL enthusiast ",
                " Love CTFs ",
-               " Flip for more info"];
+               " Flip for more info",
+               " All hail "];
 
 var icons_arr = ["<i class='fas fa-user-secret myicons'></i>",
                  "<i class='fas fa-desktop myicons' ></i>",
                  "<i class='fas fa-keyboard myicons'></i>",
                  "<i class='fas fa-dice-d20 myicons'></i>",
                  "<i class='fa fa-flag' myicons></i>",
-                 ""]
+                 "",
+                 "<span class='rick'>Pickle Rick!</span>"]
 var speed = 100;
 var myVar;
 
-function a(){
-i=0;
+function typeAnimation(){
+i = 0;
 document.getElementById("mydesc").innerHTML = "";
-typeAnimation();
+typeChars();
 }
 
-function typeAnimation() {
-
-
+function typeChars() {
 
 if(j < txt_arr.length){
 
-	txt = txt_arr[j];
+  txt = txt_arr[j];
+
   if (i < txt.length) {
     document.getElementById("mydesc").innerHTML += txt.charAt(i);
     i++;
-    myVar = setTimeout(typeAnimation, speed);
+    myVar = setTimeout(typeChars, speed);
   }
-  else{
+  else {
   i=0;
   document.getElementById("mydesc").innerHTML += icons_arr[j];
   j++;
   clearTimeout(myVar);
-  newVar = setTimeout(a,2000);
-  //document.getElementById("mydesc").innerHTML = ""
-  //setTimeout(typeAnimation, speed);
-  }}
+  newVar = setTimeout(typeAnimation,2000);
+  }
+}
  else{
  clearTimeout(myVar);
  j=0;
-  newVar = setTimeout(a,2000);
- 
+ newVar = setTimeout(typeAnimation,2000);
  }
   
 }
 
 
-a();
+typeAnimation();
+
+// TryHackMe is a superb site to learn cybersecurity.
+console.log('%c ', 'font-size:500px; background:url(https://tryhackme-badges.s3.amazonaws.com/gansta.png) no-repeat;');
+console.log("Scroll up ;P!")
